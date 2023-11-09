303af6295688eee927a3c6476f67ac16ef14595e
nc <>
2023-11-09 20:53:04 +0000

    initial meeting booking

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,214 @@
-import logo from './logo.svg';
+
+
+import React, { useState } from 'react';
 import './App.css';
+
+
+// Define the server URL
+const SERVER_URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:300';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MeetingTitleParticipantForm />
       </header>
     </div>
   );
 }
 
 export default App;
+
+function MeetingTitleParticipantForm() {
+  const [meeting, setMeeting] = useState({
+    title: '',
+    participant: { name: '', email: '' },
+    dateChoices: [{ startDateTime: '', endDateTime: '', selectedBy: '' }],
+    locationChoices: [{ location: '', selectedBy: '' }],
+    agenda: '',
+  });
+
+  const handleChange = (e) => {
+    setMeeting({ ...meeting, [e.target.name]: e.target.value });
+  };
+
+  const handleAgendaChange = (e) => {
+    setMeeting({ ...meeting, [e.target.name]: e.target.value });
+  };
+
+  const handleParticipantChange = (e) => {
+    setMeeting({
+      ...meeting,
+      participant: { ...meeting.participant, [e.target.name]: e.target.value }
+    });
+  };
+
+  const handleDateChange = (index, e) => {
+    const updatedDateChoices = meeting.dateChoices.map((choice, i) => {
+      if (i === index) {
+        return { ...choice, [e.target.name]: e.target.value };
+      }
+      return choice;
+    });
+    setMeeting({ ...meeting, dateChoices: updatedDateChoices });
+  };
+
+  const addDateChoice = () => {
+    setMeeting({
+      ...meeting,
+      dateChoices: [...meeting.dateChoices, { date: '', selectedBy: '' }]
+    });
+  };
+
+  const deleteDateChoice = (index) => {
+    if (meeting.dateChoices.length > 1) {
+      const updatedDateChoices = meeting.dateChoices.filter((_, i) => i !== index);
+      setMeeting({ ...meeting, dateChoices: updatedDateChoices });
+    }
+  };
+
+  const handleLocationChange = (index, e) => {
+    const updatedLocationChoices = meeting.locationChoices.map((choice, i) => {
+      if (i === index) {
+        return { ...choice, [e.target.name]: e.target.value };
+      }
+      return choice;
+    });
+    setMeeting({ ...meeting, locationChoices: updatedLocationChoices });
+  };
+
+  const addLocationChoice = () => {
+    setMeeting({
+      ...meeting,
+      locationChoices: [...meeting.locationChoices, { location: '', selectedBy: '' }]
+    });
+  };
+
+  const deleteLocationChoice = (index) => {
+    if (meeting.locationChoices.length > 1) {
+      const updatedLocationChoices = meeting.locationChoices.filter((_, i) => i !== index);
+      setMeeting({ ...meeting, locationChoices: updatedLocationChoices });
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`${SERVER_URL}/new`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title: meeting.title,
+          participants: [meeting.participant],
+          dateChoices: [meeting.dateChoices],
+          locationChoices: [meeting.locationChoices],
+          agenda: meeting.agenda,
+          //// Wrapping participant in an array
+        })
+      });
+
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log('Meeting created:', responseData);
+        // Handle successful response, e.g., displaying a success message or redirecting
+        
+      } else {
+        // Handle non-successful responses
+        throw new Error('Network response was not ok.');
+      }
+    } catch (error) {
+      console.error('Error during form submission:', error);
+      // Handle errors, e.g., displaying an error message to the user
+    }
+  };
+
+  return (<>
+    <svg width="100" height="50" xmlns="http://www.w3.org/2000/svg">
+      <rect x="10" y="10" width="80" height="30" fill="blue" />
+      <text x="15" y="30" font-family="Verdana" font-size="15" fill="white">Meet Me</text>
+    </svg>
+    <form onSubmit={handleSubmit}>
+
+      <div className="form-group">
+        <label>Name:</label>
+        <input type="text" name="name" value={meeting.participant.name} onChange={handleParticipantChange} />
+      </div>
+      <div className="form-group">
+        <label>Email:</label>
+        <input type="email" name="email" value={meeting.participant.email} onChange={handleParticipantChange} />
+      </div>
+
+      <div className="form-group">
+        <label>Title:</label>
+        <input type="text" name="title" value={meeting.title} onChange={handleChange} />
+      </div>
+
+      <div className="form-group">
+        <label>Agenda:</label>
+        <input type="text" name="agenda" value={meeting.agenda} onChange={handleAgendaChange} />
+      </div>
+
+      {meeting.dateChoices.map((choice, index) => (
+        <div key={index} className="form-group">
+          <label>
+            Start Date and Time:
+            <input
+              type="datetime-local"
+              name="startDateTime"
+              value={choice.startDateTime}
+              onChange={(e) => handleDateChange(index, e)}
+            />
+          </label>
+          <label>
+            End Date and Time:
+            <input
+              type="datetime-local"
+              name="endDateTime"
+              value={choice.endDateTime}
+              onChange={(e) => handleDateChange(index, e)}
+            />
+          </label>
+
+          <button type="button" onClick={addDateChoice}>Add Date Choice</button>
+          <button
+            type="button"
+            onClick={() => deleteDateChoice(index)}
+            disabled={meeting.dateChoices.length === 1}
+          >
+            Delete Date Choice
+          </button>
+        </div>
+      ))}
+
+      {meeting.locationChoices.map((choice, index) => (
+        <div key={index} className="form-group">
+          <label>
+            Location Choice:
+            <input
+              type="text"
+              name="location"
+              value={choice.location}
+              onChange={(e) => handleLocationChange(index, e)}
+            />
+          </label>
+          <button type="button" onClick={addLocationChoice}>Add Location Choice</button>
+          <button
+            type="button"
+            onClick={() => deleteLocationChoice(index)}
+            disabled={meeting.locationChoices.length === 1}
+          >
+            Delete location Choice
+          </button>
+        </div>
+      ))}
+
+      <button type="submit">Submit</button>
+    </form>
+
+  </>
+  );
+}
+
--- server/index.js
+++ server/index.js
@@ -1,18 +1,48 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const nodemailer = require('nodemailer');
+// const express = require('express');
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+// const nodemailer = require('nodemailer');
+
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import nodemailer from 'nodemailer';
+import ical from 'ical-generator';
+import cors from 'cors';
+
+
+
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  securre: false,
   auth: {
-    user: 'vierafajnor@gmail.com',
-    pass: 'Chalais2005'
+    user: 'letsmeetmybuddy@gmail.com',
+    pass: 'dbwz bsji seen byky'
   }
 });
+
+const calendar = ical({ domain: 'your-domain.com', name: 'My Calendar' });
+
+
+function createCalendarInvite(meeting) {
+
+ // const calendar = ical({ domain: 'yourdomain.com', name: 'My Calendar' });
+  calendar.createEvent({
+    start: meeting.startTime,
+    end: meeting.endTime,
+    summary: meeting.title,
+    description: 'Meeting with ' + meeting.participants.join(', '),
+    location: meeting.location
+  });
+
+  return calendar.toString();
+}
+
 
 // MongoDB URI and connection
 const uri =  'mongodb+srv://meetme:hotjava@meet.iyufxz7.mongodb.net/?retryWrites=true&w=majority'//process.env.MONGO_URI;
@@ -20,21 +50,61 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 
-// Mongoose Schema and Model for Meeting
-const meetingSchema = new mongoose.Schema({
-  title: String,
-  participants: [String], // Array of participant names
-  startTime: Date,
-  endTime: Date,
-  location: String
+// User Schema
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  }
 });
 
+// Date Choice Schema
+const dateChoiceSchema = new mongoose.Schema({
+  date: Date,
+  selectedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+});
+
+// Location Choice Schema
+const locationChoiceSchema = new mongoose.Schema({
+  location: String,
+  selectedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+});
+
+// Meeting Schema
+const meetingSchema = new mongoose.Schema({
+  title: String,
+  participants: [userSchema], // Array of users
+  dateChoices: [dateChoiceSchema], // Multiple date choices
+  locationChoices: [locationChoiceSchema], // Multiple location choices
+  startTime: Date, // Finalized start time
+  endTime: Date, // Finalized end time
+  location: String,// Finalized location
+  agenda: String // objectives
+});
+
+// Create Models
+const User = mongoose.model('User', userSchema);
 const Meeting = mongoose.model('Meeting', meetingSchema);
+
 
 // CRUD Operations for Meetings
 // Create a Meeting
 app.post('/meetings', async (req, res) => {
-  const meeting = new Meeting(req.body);
+  
+  const meeting = new Meeting({
+    title: req.body.title,
+    participants: req.body.participants, // Array of user objects
+    dateChoices: req.body.dateChoices,   // Array of date choice objects
+    locationChoices: req.body.locationChoices, // Array of location choice objects
+    startTime: req.body.startTime,       // Optionally set a fixed start time
+    endTime: req.body.endTime,           // Optionally set a fixed end time
+    location: req.body.location,
+    agenda: req.body.agenda           // Optionally set a fixed location
+  });
+
+
   try {
     const result = await meeting.save();
     res.status(201).json(result);
@@ -45,18 +115,29 @@ app.post('/meetings', async (req, res) => {
 
 // Function to generate a meeting link
 function generateMeetingLink(meeting) {
-  const baseUrl =  `http://${process.env.uri}/meetings/`;
+  const baseUrl =  `http://localhost:/meetings/`;
   return `${baseUrl}${meeting._id}`;
 }
 
 // Example usage in the Create Meeting endpoint
 app.post('/new', async (req, res) => {
   try {
-    const meeting = new Meeting(req.body);
+    console.log("req.body", req.body);
+      const meeting = new Meeting({
+      title: req.body.title,
+      participants: req.body.participants, // Array of user objects
+      dateChoices: req.body.dateChoices,   // Array of date choice objects
+      locationChoices: req.body.locationChoices, // Array of location choice objects
+      startTime: req.body.startTime,       // Optionally set a fixed start time
+      endTime: req.body.endTime,           // Optionally set a fixed end time
+      location: req.body.location,
+      agenda: req.body.agenda          // Optionally set a fixed location
+    });
     const savedMeeting = await meeting.save();
     const meetingLink = generateMeetingLink(savedMeeting);
 
-    inviteByMail(savedMeeting);
+   // inviteByMail(savedMeeting);
+   sendMeetingInvite(savedMeeting, meetingLink);
     
     res.status(201).json({ meeting: savedMeeting, link: meetingLink });
   } catch (error) {
@@ -117,15 +198,25 @@ app.delete('/meetings/:id', async (req, res) => {
 
 
 const inviteByMail= (meeting) => {
+  
+
   try {
     // ... (existing code for creating a meeting)
 
     // Send an email after the meeting is created
     const mailOptions = {
-      from: 'your-email@gmail.com',
+      from: 'vierafajnor@gmail.com',
       to: 'nchalais@gmail.com', // The email address of the participant
       subject: 'New Meeting Created',
-      text: `A new meeting titled "${meeting.title}" has been created.`
+      text: `A new meeting titled "${meeting.title}" has been created.`,
+        attachments: [
+      {
+        filename: 'invite.ics',
+        content: icsFileContent,
+        contentType: 'text/calendar'
+      }
+    ]
+      
     };
 
     transporter.sendMail(mailOptions, function(error, info){
@@ -143,7 +234,50 @@ const inviteByMail= (meeting) => {
   }
 }
 
-const port = process.env.PORT || 3000; 
+async function sendMeetingInvite(meeting,meetingLink) {
+
+  const calendar = ical({ domain: 'yourdomain.com', name: 'My Calendar' });
+  // ...rest of your code to create event...
+  
+  // Add event to the calendar
+  // Add event to the calendar
+  calendar.createEvent({
+    start: meeting.startTime,
+    end: meeting.endTime,
+    summary: meeting.title,
+    description: meeting.agenda,
+    location: meeting.location,
+    organizer: {
+      name: meeting.participants[0].name,  // The organizer's name (optional)  
+      email: meeting.participants[0].email
+    }
+  });
+
+  const icsFileContent = calendar.toString();
+
+  const mailOptions = {
+    from: 'your-email@gmail.com',
+    to: meeting.participants[0].email, // The email address of the participant
+    subject: 'New Meeting Created ' + meeting.participants[0].name,
+    text: `A new meeting titled "${meeting.title}" has been created.` +meeting.participants[0].email +" Your Meeting Link is " + meetingLink,
+      attachments: [
+    {
+      filename: 'invite.ics',
+      content: icsFileContent,
+      contentType: 'text/calendar'
+    }
+  ]
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent: ' + info.response);
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+}
+
+const port = process.env.PORT || 300; 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
