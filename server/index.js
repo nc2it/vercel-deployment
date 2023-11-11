6f55b762368db5d1e42c7ef9660c7e97542fe851
nc <>
2023-11-11 19:05:49 +0000

    welcome

--- server/index.js
+++ server/index.js
@@ -156,7 +156,7 @@ app.post('/meetings', async (req, res) => {
 
 // Function to generate a meeting link
 function generateMeetingLink(meeting) {
-  const baseUrl = `http://localhost:3000/meetme/`;
+  const baseUrl = `https://vercel-deployment-000.vercel.app/meetme/`;
   return `${baseUrl}${meeting._id}`;
 }
 
